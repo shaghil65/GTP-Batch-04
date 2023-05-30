a4e61719006a87cc17cdad0b1b92e4bb5a66b7fb
shaghil65 <shaghil.sha@gmail.com>
2023-05-30 22:32:39 +0500

    working on search functionality

--- Task8_ElasticSearch/Task8_ElasticSearch/Program.cs
+++ Task8_ElasticSearch/Task8_ElasticSearch/Program.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics.Metrics;
 using Serilog;
 using Serilog.Sinks.Elasticsearch;
-
+using Nest;
 
 namespace LogDataGenerator
 {
@@ -11,27 +11,58 @@ namespace LogDataGenerator
         static int counter = 0;
         static void Main(string[] args)
         {
+
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console()
                 .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
                 {
-                    IndexFormat = "logdata-{0:yyyy.MM.dd}",
+                    IndexFormat = "mydata-{0:yyyy.MM.dd}",
                     AutoRegisterTemplate = true
                 })
                 .CreateLogger();
-            while (true)
-            {
-                Guid id = Guid.NewGuid();
-                string randomText = GetRandomText();
+            SearchLogs("This is Random Text4");
+            //while (true)
+            //{
+            //    Guid id = Guid.NewGuid();
+            //    string randomText = GetRandomText();
 
-                Log.Information("Log ID: {LogId} - Random Text: {RandomText}", id, randomText);
+            //    Log.Information("Log ID: " + id + " - Random Text:" + randomText);
 
-                Thread.Sleep(5000); 
-            }
+            //    Thread.Sleep(5000);
+            //}
         }
         private static string GetRandomText()
         {
             return "This is Random Text" + counter++;
         }
+        private static void SearchLogs(string searchTerm)
+        {
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200"));
+            var client = new ElasticClient(settings);
+
+            var response = client.Search<string>(s => s
+                .Index("mydata-*")
+                .Query(q => q
+                    .MatchPhrase(m => m
+                        .Field("message")
+                        .Query(searchTerm)
+                    )
+                )
+            );
+
+            if (response.IsValid)
+            {
+                Console.WriteLine("Search Results:");
+                foreach (var hit in response.Hits)
+                {
+                    Console.WriteLine($"Log ID: {hit.Id}, Message: {hit.Source}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Search failed");
+            }
+        }
+
     }
 }
