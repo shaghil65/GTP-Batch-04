9b5d8facd28fdf413c2cd01768b6a21dcbb55833
shaghil65 <shaghil.sha@gmail.com>
2023-05-31 01:33:51 +0500

    Updated Task 2

--- Task2_AzureBusService/Publisher/Program.cs
+++ Task2_AzureBusService/Publisher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
 using System;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,21 +18,76 @@ namespace Publisher
 
             while (true)
             {
-                Console.WriteLine("Enter message details:");
-
                 var message = new Message();
-
-                // Take user input for message properties
                 message.Id = Guid.NewGuid();
 
-                Console.Write("Select Category: 1 for ProcessVideo 2 for ProcessImage & 3 for ProcessTimeSeriesData: ");
-                message.Category = (MessageType)int.Parse(Console.ReadLine());
+                Console.Write("Select Category: 0 for ProcessVideo, 1 for ProcessImage & 2 for ProcessTimeSeriesData: ");
+                var categoryInput = Console.ReadLine();
+                if (!int.TryParse(categoryInput, out int category))
+                {
+                    Console.WriteLine("Invalid choice. Please enter a valid category number.");
+                    continue;
+                }
 
-                Console.Write("Payload Message: ");
-                message.Payload = Console.ReadLine();
+                message.Category = (MessageType)category;
+
+                if (message.Category == MessageType.ProcessVideo)
+                {
+                    VideoPayload payload = new VideoPayload();
+                    payload.id = Guid.NewGuid();
+                    Console.Write("Enter File Name: ");
+                    payload.filename = Console.ReadLine();
+                    Console.Write("Enter File Type: ");
+                    payload.type = Console.ReadLine();
+
+                    message.Payload = JsonConvert.SerializeObject(payload);
+                }
+                else if (message.Category == MessageType.ProcessImage)
+                {
+                    ImagePayload payload = new ImagePayload();
+                    payload.id = Guid.NewGuid();
+                    Console.Write("Enter File Name: ");
+                    payload.filename = Console.ReadLine();
+                    Console.Write("Enter Codec: ");
+                    payload.codec = Console.ReadLine();
+
+                    message.Payload = JsonConvert.SerializeObject(payload);
+                }
+                else if (message.Category == MessageType.ProcessTimeSeriesData)
+                {
+                    TimeData payload = new TimeData();
+                    payload.id = Guid.NewGuid();
+                    Console.Write("Enter File Name: ");
+                    payload.filename = Console.ReadLine();
+                    Console.Write("Enter Timestamp: ");
+                    payload.timestamp = Console.ReadLine();
+
+                    message.Payload = JsonConvert.SerializeObject(payload);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please enter a valid category number.");
+                    continue;
+                }
 
                 message.CreatedTime = DateTime.Now;
                 message.SenderId = Guid.NewGuid();
+
+
+
+                //var message = new Message();
+
+                //// Take user input for message properties
+                //message.Id = Guid.NewGuid();
+
+                //Console.Write("Select Category: 1 for ProcessVideo 2 for ProcessImage & 3 for ProcessTimeSeriesData: ");
+                //message.Category = (MessageType)int.Parse(Console.ReadLine());
+
+                //Console.Write("Payload Message: ");
+                //message.Payload = Console.ReadLine();
+
+                //message.CreatedTime = DateTime.Now;
+                //message.SenderId = Guid.NewGuid();
 
 
                 // Create the message using user input
