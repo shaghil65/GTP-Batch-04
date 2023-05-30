357f4a65adabd137be147ccba178d1827978d4b0
shaghil65 <shaghil.sha@gmail.com>
2023-05-31 01:17:36 +0500

    updated task 4

---
+++ Task4_RabbitMQ_doc/Publisher/Payload.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    public class Payload
+    {
+        public Guid id { get; set; }
+        public string filename { get; set; }
+
+    }
+    public class VideoPayload : Payload
+    {
+        public string type { get; set; }
+
+    }
+    public class ImagePayload : Payload
+    {
+        public string codec { get; set; }
+    }
+    public class TimeData : Payload
+    {
+        public string timestamp { get; set; }
+    }
+}
--- Task4_RabbitMQ_doc/Publisher/Program.cs
+++ Task4_RabbitMQ_doc/Publisher/Program.cs
@@ -1,7 +1,7 @@
-﻿using Newtonsoft.Json;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
 using System;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Publisher
 {
@@ -18,27 +18,66 @@ namespace Publisher
             while (true)
             {
                 var message = new Message();
-
-                // Take user input for message properties
                 message.Id = Guid.NewGuid();
 
-                Console.Write("Select Category: 1 for ProcessVideo 2 for ProcessImage & 3 for ProcessTimeSeriesData: ");
-                message.Category = (MessageType)int.Parse(Console.ReadLine());
+                Console.Write("Select Category: 0 for ProcessVideo, 1 for ProcessImage & 2 for ProcessTimeSeriesData: ");
+                var categoryInput = Console.ReadLine();
+                if (!int.TryParse(categoryInput, out int category))
+                {
+                    Console.WriteLine("Invalid choice. Please enter a valid category number.");
+                    continue;
+                }
 
-                Console.Write("Payload Message: ");
-                message.Payload = Console.ReadLine();
+                message.Category = (MessageType)category;
+
+                if (message.Category == MessageType.ProcessVideo)
+                {
+                    VideoPayload payload = new VideoPayload();
+                    payload.id = Guid.NewGuid();
+                    Console.Write("Enter File Name: ");
+                    payload.filename = Console.ReadLine();
+                    Console.Write("Enter File Type: ");
+                    payload.type = Console.ReadLine();
+
+                    message.Payload = JsonConvert.SerializeObject(payload);
+                }
+                else if (message.Category == MessageType.ProcessImage)
+                {
+                    ImagePayload payload = new ImagePayload();
+                    payload.id = Guid.NewGuid();
+                    Console.Write("Enter File Name: ");
+                    payload.filename = Console.ReadLine();
+                    Console.Write("Enter Codec: ");
+                    payload.codec = Console.ReadLine();
+
+                    message.Payload = JsonConvert.SerializeObject(payload);
+                }
+                else if (message.Category == MessageType.ProcessTimeSeriesData)
+                {
+                    TimeData payload = new TimeData();
+                    payload.id = Guid.NewGuid();
+                    Console.Write("Enter File Name: ");
+                    payload.filename = Console.ReadLine();
+                    Console.Write("Enter Timestamp: ");
+                    payload.timestamp = Console.ReadLine();
+
+                    message.Payload = JsonConvert.SerializeObject(payload);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please enter a valid category number.");
+                    continue;
+                }
 
                 message.CreatedTime = DateTime.Now;
                 message.SenderId = Guid.NewGuid();
 
-                // Add message to the database
                 using (var dbContext = new ApplicationDbContext())
                 {
                     dbContext.Messages.Add(message);
                     dbContext.SaveChanges();
                 }
 
-                // Convert message to JSON
                 var messageJson = JsonConvert.SerializeObject(message);
                 var body = Encoding.UTF8.GetBytes(messageJson);
 
